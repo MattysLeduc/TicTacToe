7728a5d6754106930c96aee9647b94fb5e6143c7
MattysLeduc <mattys.leduc@gmail.com>
2024-11-06 11:29:29 -0500

    Created the startup window Created the PlayerEnum Created the Board initilization

---
+++ TicTacToe/Board.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Board
+    {
+        PlayerEnum[,] board = new PlayerEnum[3,3];
+
+        public Board()
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                for(int y = 0; y < 3; y++)
+                {
+                    board[x, y] = PlayerEnum.NONE;
+                }  
+            }
+        }
+    }
+}
